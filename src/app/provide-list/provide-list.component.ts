783c98e336bf511911b0431eb1354e2a6a4cc672
Arne Siemens <arsiemens@gmail.com>
2018-03-28 15:29:52 +0200

    Style components; Add configfiles

--- src/app/provide-list/provide-list.component.ts
+++ src/app/provide-list/provide-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.Emulated,  // default
   selector: 'app-provide-list',
   templateUrl: './provide-list.component.html',
   styleUrls: ['./provide-list.component.css']
 })
 export class ProvideListComponent implements OnInit {
+  isMousedown;
+
   // tslint:disable-next-line:member-ordering
   @Input() message;
 
